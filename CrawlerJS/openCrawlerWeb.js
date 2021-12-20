9782ea5c0430ba3e4a123d5c0ab73a53677dec68
GuoHao Zeng <guohao.zeng@winwayglobal.com>
2021-12-20 16:06:38 +0800

    <feat> Add log message in openCrawlerWeb.js

--- CrawlerJS/openCrawlerWeb.js
+++ CrawlerJS/openCrawlerWeb.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const webDriver = require('selenium-webdriver');
 
 async function openCrawlerWeb(){
+    console.log('2');
     let driver = await new webDriver.Builder().forBrowser("chrome").build();
-
+    console.log('4');
     const openWeb = process.env.TargetWebURL;
+    console.log('5');
     driver.get(openWeb);
-
+    console.log('6');
 }
 
-openCrawlerWeb();
+console.log('1');
+openCrawlerWeb().then();
+console.log('3');
