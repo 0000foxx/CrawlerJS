e4a79bdd171c03e286b662269c7f7b1977fbe0fe
GuoHao Zeng <guohao.zeng@winwayglobal.com>
2021-12-20 10:15:54 +0800

    <feat> Add .env file and TargetWebURL

--- CrawlerJS/openCrawlerWeb.js
+++ CrawlerJS/openCrawlerWeb.js
@@ -5,8 +5,9 @@ const webDriver = require('selenium-webdriver');
 async function openCrawlerWeb(){
     let driver = await new webDriver.Builder().forBrowser("chrome").build();
 
-    const openWeb = 'https://writingfoxx.com';
+    const openWeb = process.env.TargetWebURL;
     driver.get(openWeb);
+
 }
 
 openCrawlerWeb();
